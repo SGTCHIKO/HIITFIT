0b40e58d68396c703be460406bb284407ed1ec6f
sgtchiko <bfr.delgado@gmail.com>
2023-11-08 23:04:41 -0500

    Initial Commit

---
+++ HIITFIT/HIITFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFITApp.swift
+//  HIITFIT
+//
+//  Created by Bryan F Rosado Delgado on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
